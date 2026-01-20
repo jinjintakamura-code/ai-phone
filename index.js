a9833008c44c0cc071c6f6f63c63eaa211fbf316
jinjintakamura-code <jinjintakamura@gmail.com>
2026-01-20 11:33:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/voice", async (req, res) => {
   res.send(`
     <Response>
       <Gather input="speech"
-              action="https://github.com/jinjinyakamura-code/aiphone/edit/main/index.js/voice"
+              action="https://github.com/jinjintakamura-code/aiphone/edit/main/index.js/voice"
               language="ja-JP"
               timeout="5">
         <Say language="ja-JP">${reply}</Say>
